77a6e92d3d76a4c4b8cc6fd7aaa3894399ac92cb
yazanabedo112 <yazanabedo112@gmail.com>
2025-04-29 00:29:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('8062347534:AAHAyTRw3EJSf6-BFWm55qTQJnISYqepDaI')
 DB_PATH = os.getenv('DB_PATH', 'bot.db')
 TRANSACTION_TIMEOUT_HOURS = int(os.getenv('TRANSACTION_TIMEOUT_HOURS', 24))
 ADMIN_IDS = list(map(int, os.getenv('ADMIN_IDS', '').split(',')))
